472c45a2ca9c37753355d6a88ab124a96358a06d
Dustin DeYoung <dustin@westlakedesign.com>
2012-06-15 09:43:56 -0400

    Setup rspec

--- test/spud_videos_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SpudVideosTest < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, SpudVideos
-  end
-end
--- test/unit/spud_video_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class SpudVideoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
