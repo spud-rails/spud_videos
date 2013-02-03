2bb017d75d18a09cc078f18256df0947e88f99fb
David Estes <destes@redwindsw.com>
2013-02-03 09:19:14 -0500

    prepping for brightcove

--- app/helpers/spud_videos_helper.rb
+++ app/helpers/spud_videos_helper.rb
@@ -1,3 +1,4 @@
+require 'cgi'
 module SpudVideosHelper
 
 	def spud_video(set_or_identifier,options = {})
@@ -20,6 +21,28 @@ module SpudVideosHelper
 			source = "http://player.vimeo.com/video/#{extracted_video_id}?title=0&amp;byline=0&amp;portrait=0"
 			content = "<iframe src='#{source}' width='#{width}' height='#{height}' frameborder='0' webkitAllowFullScreen mozallowfullscreen allowfullscreen></iframe>"
 			return content.html_safe
+    elsif video.source == 'brightcove'
+      parameters = CGI::parse(video.embed_link)
+      content="<!-- Start of Brightcove Player -->
+        <div style=\"display: none;\"></div>
+        <!-- By use of this code snippet, I agree to the Brightcove Publisher T and C  found at https://accounts.brightcove.com/en/terms-and-conditions/.  -->
+        <p><script language=\"JavaScript\" type=\"text/javascript\" src=\"http://admin.brightcove.com/js/BrightcoveExperiences.js\"></script> <object id=\"myExperience2124779161001\" class=\"BrightcoveExperience\">
+          <param name=\"bgcolor\" value=\"#FFFFFF\" />
+          <param name=\"width\" value=\"#{width}\" />
+          <param name=\"height\" value=\"#{height}\" />
+
+          <param name=\"isVid\" value=\"true\" />
+          <param name=\"isUI\" value=\"true\" />
+          <param name=\"dynamicStreaming\" value=\"true\" />
+        "
+        parameters.each do |key,value|
+          content += "<param name=\"#{key}\" value=\"#{value}\" />"
+        end
+        content += "</object></p>
+        <!--  This script tag will cause the Brightcove Players defined above it to be created as soon as the line is read by the browser. If you wish to have the player instantiated only after the rest of the HTML is processed and the page load is complete, remove the line. -->
+        "
+
+        return content.html_save
 		end
 	end
 end
